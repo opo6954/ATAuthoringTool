de445c7430281465671efb7b8100c86c682dabfd
JinkiJung <hypnotize@kaist.ac.kr>
2017-03-02 21:15:03 +0900

    working on environment editing(ongoing)

---
+++ VR_Authoring/Assets/Script/Environment/EnvironmentManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentManager : MonoBehaviour {
+    public ThalmicMyo myoRight;
+    public GameObject spatialMapping;
+    public GameObject canvas;
+
+	// Use this for initialization
+	void Start () {
+
+        canvas.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(myoRight.pose == Thalmic.Myo.Pose.FingersSpread)
+        {
+            ShowMenu(true);
+        }
+
+        /*
+        if (myoRight.pose == Thalmic.Myo.Pose.Fist)
+        {
+            ShowMenu(false);
+        }
+        */
+    }
+
+    public void ShowMenu(bool turnOn)
+    {
+        if (turnOn)
+        {
+            spatialMapping.SetActive(false);
+            canvas.SetActive(true);
+            canvas.GetComponent<FloatingCanvas>().RepositionCanvas();
+        }
+        else
+        {
+            spatialMapping.SetActive(true);
+            canvas.SetActive(false);
+        }
+    }
+}

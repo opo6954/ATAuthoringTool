b66d46355ad7421084055f67d7d2de7033994346
JinkiJung <hypnotize@kaist.ac.kr>
2017-03-02 11:26:35 +0900

    Myo integration and gesture input implementation

---
+++ VR_Authoring/Assets/Script/Interface/GestureInput.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestureInput {
+    public enum GestureType
+    {
+        MOUSE_L_DOWN =0,
+        MOUSE_L_PRESS = 1,
+        MOUSE_L_UP = 2,
+        MOUSE_R_DOWN = 3,
+        MOUSE_R_PRESS = 4,
+        MOUSE_R_UP = 5,
+        MYO_FIST= 6,
+        MYO_SPREAD = 7,
+        MYO_WAVE_IN = 8,
+        MYO_WAVE_OUT = 9,
+        MYO_REST = 10,
+        MYO_D_TAP = 11,
+        KEY_SPACE = 12
+    }
+
+    public int InputID;
+
+    public virtual bool Next() { return false;  }
+    public virtual bool Hold() { return false; }
+    public virtual bool Pause() { return false; }
+    public virtual bool Cancel() { return false; }
+}
---
+++ VR_Authoring/Assets/Script/Interface/MyoInput.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Thalmic.Myo;
+
+public class MyoInput: GestureInput {
+    public MyoGesture myo;
+
+    public override bool Next() { return myo.currentGesture == MyoGesture.Gesture.CLICK; }
+    public override bool Hold() { return myo.currentGesture == MyoGesture.Gesture.FIST_HOLD; }
+    public override bool Pause() { return myo.currentGesture == MyoGesture.Gesture.SPREAD_HOLD; }
+    public override bool Cancel() { return myo.currentGesture == MyoGesture.Gesture.DOUBLE_TAP; }
+}

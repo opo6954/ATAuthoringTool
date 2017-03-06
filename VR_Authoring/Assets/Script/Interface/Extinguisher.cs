ac1cab545f82c9a8b28d3eebd6e714a3fa1fcf83
JinkiJung <hypnotize@kaist.ac.kr>
2017-03-06 11:57:16 +0900

    fire extinguishment implementation (ongoing)

--- VR_Authoring/Assets/Script/Interface/Extinguisher.cs
+++ VR_Authoring/Assets/Script/Interface/Extinguisher.cs
@@ -5,15 +5,60 @@ using Thalmic.Myo;
 
 public class Extinguisher: MonoBehaviour {
     public MyoGesture myo;
+    public GameObject smokeEffect;
+    public GameObject smokeSound;
 
+    UnityEngine.Vector3 inactivatedPosition;
+    float step;
+    float offset = 0.05f;
     public void Start()
     {
+        inactivatedPosition = new UnityEngine.Vector3(0, -0.1f, 0);
+        step = 1.0f;
+    }
 
+    void Activate()
+    {
+        // adjustment of myo rotation
+        UnityEngine.Quaternion myoQuat = UnityEngine.Quaternion.Euler(myo.myo.gyroscope);
+        UnityEngine.Quaternion relative = UnityEngine.Quaternion.Inverse(this.transform.rotation) * myoQuat;
+        this.transform.localRotation = UnityEngine.Quaternion.Slerp(UnityEngine.Quaternion.identity, UnityEngine.Quaternion.Inverse(myoQuat), 0.2f);
+
+        // smooth translation
+        this.transform.localPosition = UnityEngine.Vector3.Lerp(UnityEngine.Vector3.zero, inactivatedPosition, step);
+        if (step > 0.0f)
+        {
+            step -= offset;
+        }
+        if(!smokeEffect.active)
+            smokeEffect.SetActive(true);
+        if (!smokeSound.active)
+            smokeSound.SetActive(true);
+    }
+
+    void Deactivate()
+    {
+        this.transform.localPosition = UnityEngine.Vector3.Lerp(UnityEngine.Vector3.zero, inactivatedPosition, step);
+        if (step < 1.0f)
+        {
+            step += offset;
+        }
+        this.transform.localRotation = UnityEngine.Quaternion.Slerp(this.transform.localRotation, UnityEngine.Quaternion.identity, 0.2f);
+        if (smokeEffect.active)
+            smokeEffect.SetActive(false);
+        if (smokeSound.active)
+            smokeSound.SetActive(false);
     }
 
     private void Update()
     {
         if(myo.myo.pose == Thalmic.Myo.Pose.Fist)
-            this.transform.eulerAngles = myo.myo.gyroscope;
+        {
+            Activate();
+        }
+        else
+        {
+            Deactivate();
+        }
     }
 }

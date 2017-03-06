2c6fd1bdc6a79c4fe803ad4828eb64e999dd0ba7
JinkiJung <hypnotize@kaist.ac.kr>
2017-03-06 14:34:52 +0900

    complete implementation of fire extinguishment

---
+++ VR_Authoring/Assets/Delete.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delete : MonoBehaviour {
+    public ParticleSystem part;
+    public List<ParticleCollisionEvent> collisionEvents;
+    public FireEffect fe;
+    // Use this for initialization
+    void Start () {
+        part = GetComponent<ParticleSystem>();
+        collisionEvents = new List<ParticleCollisionEvent>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnParticleCollision(GameObject other)
+    {
+        fe.Attacked();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        fe.Attacked();
+    }
+}
---
+++ VR_Authoring/Assets/Script/Environment/FireEffect.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireEffect : MonoBehaviour {
+    public int life = 100;
+
+    public GameObject fire_large;
+    public GameObject fire_middle;
+    public GameObject fire_die;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(life>50)
+        {
+            fire_large.SetActive(true);
+            fire_middle.SetActive(false);
+            fire_die.SetActive(false);
+        }
+        else if (life > 0)
+        {
+            fire_large.SetActive(false);
+            fire_middle.SetActive(true);
+            fire_die.SetActive(false);
+        }
+        else
+        {
+            fire_large.SetActive(false);
+            fire_middle.SetActive(false);
+            fire_die.SetActive(true);
+        }
+	}
+
+    public void Attacked()
+    {
+        if (life > 0)
+            life--;
+    }
+}
